294fdc19732c65bcfe13f8c5fc5a288d76ac396d
Вадим Гусейнов <167712090+JuliaDJ1@users.noreply.github.com>
2026-05-11 16:54:36 +0000

    fix pep8 issues

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TagViewSet, IngredientViewSet, RecipeViewSet, SubscriptionViewSet, UserViewSet, AvatarView
+from .views import (
+    TagViewSet, IngredientViewSet, RecipeViewSet,
+    SubscriptionViewSet, UserViewSet, AvatarView
+)
 
 router = DefaultRouter()
 router.register('tags', TagViewSet)
@@ -8,8 +11,16 @@ router.register('ingredients', IngredientViewSet)
 router.register('recipes', RecipeViewSet)
 
 urlpatterns = [
-    path('users/subscriptions/', SubscriptionViewSet.as_view({'get': 'subscriptions'}), name='subscriptions'),
-    path('users/<int:pk>/subscribe/', SubscriptionViewSet.as_view({'post': 'subscribe', 'delete': 'subscribe'}), name='subscribe'),
+    path(
+        'users/subscriptions/',
+        SubscriptionViewSet.as_view({'get': 'subscriptions'}),
+        name='subscriptions'
+    ),
+    path(
+        'users/<int:pk>/subscribe/',
+        SubscriptionViewSet.as_view({'post': 'subscribe', 'delete': 'subscribe'}),
+        name='subscribe'
+    ),
     path('users/<int:pk>/', UserViewSet.as_view({'get': 'retrieve'}), name='user-detail'),
     path('users/me/avatar/', AvatarView.as_view(), name='avatar'),
     path('', include(router.urls)),
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Tag(models.Model):
     name = models.CharField('Название', max_length=200, unique=True)
     slug = models.SlugField('Slug', max_length=200, unique=True)
